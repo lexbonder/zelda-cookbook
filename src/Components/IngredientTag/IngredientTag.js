747cd57a5d2d9234c8e5852569334a002a562ae2
Alex Bonder <lexbonder@gmail.com>
2018-04-12 15:11:39 -0600

    Add reasonable linter

--- src/Components/IngredientTag/IngredientTag.js
+++ src/Components/IngredientTag/IngredientTag.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 import React from 'react';
 import PropTypes from 'prop-types';
 import './IngredientTag.css';
--- src/Components/RecipeCard/RecipeCard.js
+++ src/Components/RecipeCard/RecipeCard.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 import React from 'react';
 import './RecipeCard.css';
 import PropTypes from 'prop-types';
