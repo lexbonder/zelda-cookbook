d9f14dcfd6cb2941b6157cd4c5757dfa6b7aa0e8
Jeff Bender <j25bender@gmail.com>
2018-04-12 20:07:14 -0600

    filtering by type button

--- src/Components/TypeTabs/TypeTabs.js
+++ src/Components/TypeTabs/TypeTabs.js
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { populateRecipes } from '../../actions'
 // import PropTypes from 'prop-types';
 import './TypeTabs.css';
 
-const TypeTabs = () => (
-    <nav onClick={(e) => tabClicked(e)} >
-      <ul>
-      <li><a id="restore-hearts">Restore Hearts</a></li>
-      <li><a id="restore-stamina">Restore Stamina</a></li>
-      <li><a id="cold-resist">Cold Resist</a></li>
-      <li><a id="heat-resist">Heat Resist</a></li>
-      <li><a id="electric-resist">Electric Resist</a></li>
-      <li><a id="movement-speed">Movement Speed</a></li>
-      <li><a id="temp-max-hearts">Temp Max Hearts</a></li>
-      <li><a id="defense-boost">Defense Boost</a></li>
-      <li><a id="attack-power">Attack Power</a></li>
-      <li><a id="stealth">Stealth Recipes</a></li>
-      <li><a id="elixers">Elixer Recipes</a></li>
-      </ul>
-    </nav>
-)
+export class TypeTabs extends Component {
+  constructor() {
+    super()
+  }
 
-const tabClicked = (e) => {
-  const targetTab = e.target.id
-  const effect = targetTab.replace('-', ' ')
-  console.log(effect)
+  tabClicked = (e) => {
+    const effect = e.target.id.replace('-', ' ')
+    const allRecipes = this.props.recipes
+    
+    const matchingRecipes = allRecipes.filter(recipe => {
+      let lowerCaseType = recipe.type.toLowerCase()
+      return lowerCaseType.includes(effect)
+    })
+    this.props.populateRecipes(matchingRecipes)
+  }
+
+  render() {
+    return (
+      <nav onClick={(e) => this.tabClicked(e)} >
+        <ul>
+        <li><a id="restore-hearts">Restore Hearts</a></li>
+        <li><a id="stamina">Restore Stamina</a></li>
+        <li><a id="cold">Cold Resist</a></li>
+        <li><a id="heat">Heat Resist</a></li>
+        <li><a id="electric">Electric Resist</a></li>
+        <li><a id="speed">Movement Speed</a></li>
+        <li><a id="temp">Temp Max Hearts</a></li>
+        <li><a id="defense">Defense Boost</a></li>
+        <li><a id="attack">Attack Power</a></li>
+        <li><a id="stealth">Stealth Recipes</a></li>
+        <li><a id="elixer">Elixer Recipes</a></li>
+        </ul>
+      </nav>
+    )
+  } 
 }
 
-export default TypeTabs;
+export const MSTP = ({ recipes }) => ({
+  recipes
+})
+
+export const MDTP = dispatch => ({
+  populateRecipes: matchingRecipes => dispatch(populateRecipes(matchingRecipes))
+})
+
+export default connect(MSTP, MDTP)(TypeTabs)
