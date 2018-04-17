b9190dc380a666ed64f3a6d38faf7ca14f5ff148
Jeff Bender <j25bender@gmail.com>
2018-04-17 12:41:28 -0600

    cleaned up css and scss removed empty css files

--- src/Containers/NameFilter/NameFilter.js
+++ src/Containers/NameFilter/NameFilter.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { updateNameFilter } from '../../actions';
-import './NameFilter.css';
 
 let timeout;
 
